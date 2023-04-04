ecee0cef66cdfd25d4f0ac322bfbb04be5406835
Grigory <grinmorg@gmail.com>
2023-04-04 11:06:42 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Home from "./pages/Home";
+// import Canvas from "./canvas";
+// import Customizer from "./pages/Customizer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      {/* <Canvas /> */}
+      {/* <Customizer /> */}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/Customizer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Customizer = (props: Props) => {
+  return <div>Customizer</div>;
+};
+
+export default Customizer;
